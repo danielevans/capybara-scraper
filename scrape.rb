f80503737fc7175ac98f8155f1c518fc863d9102
Daniel Evans <evans.daniel.n@gmail.com>
2013-09-04 17:11:50 -0600

    Stage 2

--- scrape.rb
+++ scrape.rb
@@ -1,14 +1,31 @@
 require 'rubygems'
-
+require 'bundler/setup'
+require 'json'
+require 'securerandom'
 require './capybara.rb'
 
 target = ENV["target"] || "http://reddit.com/r/funny"
+downloaded_images = {}
+
+if File.exist? "downloaded.json"
+  downloaded_images = JSON.parse(File.open("downloaded.json", "r").read)
+end
 
 include Capybara::DSL
 
 visit target
 
-save_screenshot('screenshot.png')
+new_images = all("a").map { |a| a["href"] || a["src"] }.compact.uniq.select do |url|
+  URI.parse(url).path.match(/\.(jpg|png|gif)$/) && !downloaded_images.has_key?(url) rescue false
+end.map do |url|
+  filename = "#{SecureRandom.hex.gsub(/=/, '')}.#{URI.parse(url).path.match(/\.(jpg|png|gif)$/)[1]}"
+  `wget "#{url}" -O downloaded/#{filename}`
+  downloaded_images[url] = true if $?.to_i == 0
+  filename
+end
 
-# `open screenshot.png`
-`feh -Z -F --cycle-once screenshot.png`
+File.open("downloaded.json", "wb") do |f|
+  f.write downloaded_images.to_json
+end
+
+`feh -Z -F --cycle-once downloaded/#{new_images.join(' downloaded/')}` unless new_images.empty?
